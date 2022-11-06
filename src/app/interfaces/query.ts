927ddf01d6bec6f2da40a2ffd9647bb6f0dfe6db
Fabricio Winter <git@notfab.net>
2022-11-06 01:45:25 -0300

    Remove unused services and models

--- src/app/interfaces/query.ts
+++ src/app/interfaces/query.ts
@@ -1,14 +1,14 @@
 import {User} from "./user";
 
-export class Query {
+export interface Query {
 
-  id!: string;
-  requester!: User;
+  id: string,
+  requester: User,
 
   // connection
 
-  query!: string;
-  description!: string;
-  status!: "WAITING_REVIEW" | "APPROVED" | "REJECTED" | "EXECUTING" | "DONE";
+  query: string,
+  description: string,
+  status: "WAITING_REVIEW" | "APPROVED" | "REJECTED" | "EXECUTING" | "DONE"
 
 }

b94ffd794d61ba0d81c777a369ffab5af36348a4
Tiago Valdrich <tiago.valdrich@hotmail.com>
2022-11-10 20:31:20 -0300

    fix: application name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ScriptSql'`, () => {
+  it(`should have as title 'ScriptQL'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ScriptSql');
+    expect(app.title).toEqual('ScriptQL');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ScriptSql app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ScriptQL app is running!');
   });
 });
